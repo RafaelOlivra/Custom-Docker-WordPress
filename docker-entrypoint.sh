ca67e7d2a6ac90b1dd1b946f37cbc6c9a900eef5
Rafael Oliveira <rafael@doting.com.br>
2022-10-28 09:03:15 -0300

    Added snakeoil cert fallback option to allow https on local environments

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,22 +2,31 @@
 set -Eeuo pipefail
 
 # Install apache if needed
-if [[ ! -f /usr/sbin/apache2 ]]
-then
-apt update
-apt install -y apache2
+if [[ ! -f /usr/sbin/apache2 ]]; then
 
-# Set Apache variables
-echo "ServerName $DOMAIN" >> /etc/apache2/apache2.conf
-echo "Define ServerName $DOMAIN" >> /etc/apache2/conf-enabled/environment.conf
+    apt update
+    apt install -y apache2
 
-# Enable apache modules & php-fpm config
-a2enmod http2 ssl mpm_event rewrite headers deflate expires brotli proxy_fcgi setenvif
-a2enconf php-fpm
-a2ensite $DOMAIN.conf
+    # Set Apache variables
+    echo "ServerName $DOMAIN" >> /etc/apache2/apache2.conf
+    echo "Define ServerName $DOMAIN" >> /etc/apache2/conf-enabled/environment.conf
 
-# Remove the default index.html file added by the initial apache install
-rm -f /var/www/html/index.html
+    # Enable apache modules & php-fpm config
+    a2enmod http2 ssl mpm_event rewrite headers deflate expires brotli proxy_fcgi setenvif
+    a2enconf php-fpm
+    a2ensite $DOMAIN.conf
+
+    # Remove the default index.html file added by the initial apache install
+    rm -f /var/www/html/index.html
+fi
+
+# Setup snakeoil cert symlinks if needed
+if [[ ! -f /var/imported/ssl/live/$DOMAIN/fullchain.pem ]]; then
+    if [ $USE_SNAKEOIL_CERT_FALLBACK == 'true' ]; then
+        mkdir -p /var/imported/ssl/live/$DOMAIN/
+        ln -s /etc/ssl/certs/ssl-cert-snakeoil.pem /var/imported/ssl/live/$DOMAIN/fullchain.pem
+        ln -s /etc/ssl/private/ssl-cert-snakeoil.key /var/imported/ssl/live/$DOMAIN/privkey.pem
+    fi
 fi
 
 # Go live!
